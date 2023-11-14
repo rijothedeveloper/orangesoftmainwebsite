334a44403cf618a7c4d8c9fd879812b6eb1a45d1
Rijo George <rijogeorge@MacBook-Pro.local>
2023-11-14 12:19:09 -0800

    hero content basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
 import './App.css'
 import video from './assets/video/hero_video.mp4'
+import logo from './assets/images/logo.png'
 
 function App() {
 
   return (
     <>
-        <video id="background-video" autoPlay loop muted poster="https://assets.codepen.io/6093409/river.jpg">
-            <source src={video} type="video/mp4" />
-        </video>
-        <h1>THIS IS A RIVER.</h1>
-        <h2>How majestic.</h2>
+
+            <video className="hero-section background-video" autoPlay loop muted poster="https://assets.codepen.io/6093409/river.jpg">
+                <source src={video} type="video/mp4" />
+            </video>
+            <div className="hero-section overlay"></div>
+            <section className="hero-content" >
+                <div className="nav-section">
+                    <div >
+                    <img src={logo} />
+                    </div>
+                    <div className="navbar">
+                        <ul>
+                            <li><a href="">Services</a></li>
+                            <li><a href="">Info</a></li>
+                            <li><a href="">Reviews</a></li>
+                            <li><a href="">Blog</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <h1>We help small business + nonprofits get online.</h1>
+                <h2>We build affordable SEO friendly website and offer an amazing digital marketing</h2>
+                <h2>We create content and videos that helps you connect with your target customers.</h2>
+                <div className="check-work-button">
+                    Check Out Our Work
+                </div>
+            </section>
+
+
     </>
   )
 }
