e2b6d44b376f3e928fae431d5defbcabeba650a7
Rijo George <rijogeorge@MacBook-Pro.local>
2023-11-14 09:58:50 -0800

    main hero video added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import video from './assets/video/hero_video.mp4'
 
 function App() {
 
   return (
     <>
-      dummy
+        <video id="background-video" autoPlay loop muted poster="https://assets.codepen.io/6093409/river.jpg">
+            <source src={video} type="video/mp4" />
+        </video>
+        <h1>THIS IS A RIVER.</h1>
+        <h2>How majestic.</h2>
     </>
   )
 }
